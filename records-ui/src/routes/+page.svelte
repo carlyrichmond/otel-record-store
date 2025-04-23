d695d540294b123a7bf4884e2ab78627d622da36
Carly Richmond <74931905+carlyrichmond@users.noreply.github.com>
2025-04-23 20:00:34 +0100

    Adding RUM instrumentation

--- records-ui/src/routes/+page.svelte
+++ records-ui/src/routes/+page.svelte
@@ -1,13 +1,17 @@
 <script>
-	import { onMount } from 'svelte';
-    import { ClientTelemetry } from '$lib/telemetry/frontend.tracer';
-	//import { WebVitalsInstrumentation } from '$lib/telemetry/web-vitals.instrumentation';
     import Featured from '../components/featured/featured.svelte';
 
-    //WebVitalsInstrumentation.init();
-    /*onMount(() => {
-		ClientTelemetry.getInstance().start();
-	});*/
+    // RUM Initialisation
+    import { init as initApm } from '@elastic/apm-rum'
+    
+    const apm = initApm({
+        serviceName: 'rum-records-ui',
+        serverUrl: 'https://50bdcd88eee64c8a9f4cd27bcba2aa14.apm.eu-west-2.aws.cloud.es.io:443',
+        distributedTracingOrigins: ['http://localhost:8080'],
+        serviceVersion: '1.0.0',
+        environment: 'dev'
+});
+    
 
 </script>
 
