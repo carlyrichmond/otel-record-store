921ffa1bff007596a4c35dbfeda2c3129596688f
Carly Richmond <carly.richmond@elastic.co>
2025-07-21 14:45:22 +0100

    Adding well-known exclusion

--- records-ui/src/hooks.server.ts
+++ records-ui/src/hooks.server.ts
@@ -3,5 +3,9 @@ import { Telemetry } from '$lib/telemetry/node.instrumentation';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	//Telemetry.getInstance();
+	if (event.url.pathname.startsWith('/.well-known/appspecific/com.chrome.devtools')) {
+		return new Response(null, { status: 204 }); // Return empty response with 204 No Content
+	}
+
 	return resolve(event);
 };
