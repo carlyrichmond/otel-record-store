83b57b9fa62fc00c85117ed25df10c598af878eb
Carly Richmond <carly.richmond@elastic.co>
2025-08-06 13:59:42 +0100

    Removing RUM

--- records-ui/src/routes/+page.svelte
+++ records-ui/src/routes/+page.svelte
@@ -1,17 +1,5 @@
 <script>
     import Featured from '../components/featured/featured.svelte';
-
-    // RUM Initialisation
-    import { init as initApm } from '@elastic/apm-rum';
-    
-    const apm = initApm({
-        serviceName: 'rum-records-ui',
-        serverUrl: 'https://50bdcd88eee64c8a9f4cd27bcba2aa14.apm.eu-west-2.aws.cloud.es.io:443',
-        distributedTracingOrigins: ['http://localhost:8080'],
-        serviceVersion: '1.0.0',
-        environment: 'dev'
-});
-
 </script>
 
 <Featured/>
